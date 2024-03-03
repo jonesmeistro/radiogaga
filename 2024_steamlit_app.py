b9ba039de582c06f439e110434011bd8c4324142
jonesmeistro <162049699+jonesmeistro@users.noreply.github.com>
2024-03-03 19:15:31 +0000

    Update 2024_steamlit_app.py

--- 2024_steamlit_app.py
+++ 2024_steamlit_app.py
@@ -9,7 +9,7 @@ Original file is located at
 
 import openai
 import os 
-from pinecone import Pinecone
+from pinecone import Pinecone, ServerlessSpec
 
 # Initialize API keys
 openai.api_key = os.getenv("OPENAI_API_KEY")
